dbafd1c896686c4d706cff4cee27139573bc4fc7
Jules Lanari-Collard <jules_collard@yahoo.co.uk>
2025-01-12 17:12:42 -0500

    Players table

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, date
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
@@ -14,4 +14,47 @@ class Team(db.Model):
     metaDateTime: so.Mapped[datetime] = so.mapped_column(default = lambda: datetime.now(timezone.utc))
 
     def __repr__(self):
-        return f'<{self.fullName}>'
+        return f"<{self.fullName}>"
+    
+
+class Player(db.Model):
+    __tablename__ = 'players'
+    
+    """ NOT WORKING
+    __tableargs__ = (sa.CheckConstraint('''isActive IN (0,1)'''),
+                     sa.CheckConstraint("sweaterNumber >= 0"),
+                     sa.CheckConstraint("heightInInches >= 0"),
+                     sa.CheckConstraint("heightInCentimeters >= 0"),
+                     sa.CheckConstraint("weightInPounds >= 0"),
+                     sa.CheckConstraint("weightInKilograms >= 0"),
+                     sa.CheckConstraint('''shootsCatches IN ("L","R")'''),
+                     sa.CheckConstraint('''isActive IN (0,1)'''))
+    """
+                     
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    isActive: so.Mapped[int] = so.mapped_column()
+    currentTeamID: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Team.id))
+    firstName: so.Mapped[str] = so.mapped_column()
+    lastName: so.Mapped[str] = so.mapped_column()
+    sweaterNumber: so.Mapped[int] = so.mapped_column(nullable=True)
+    position: so.Mapped[str] = so.mapped_column()
+    headshot: so.Mapped[str] = so.mapped_column()
+    heroImage: so.Mapped[str] = so.mapped_column()
+    heightInInches: so.Mapped[int] = so.mapped_column(nullable=True)
+    heightInCentimeters: so.Mapped[int] = so.mapped_column(nullable=True)
+    weightInPounds: so.Mapped[int] = so.mapped_column(nullable=True)
+    weightInKilograms: so.Mapped[int] = so.mapped_column(nullable=True)
+    birthDate: so.Mapped[date] = so.mapped_column()
+    birthCity: so.Mapped[str] = so.mapped_column(nullable=True)
+    birthCountry: so.Mapped[str] = so.mapped_column(nullable=True)
+    shootsCatches: so.Mapped[str] = so.mapped_column()
+    draftYear: so.Mapped[int] = so.mapped_column(nullable=True)
+    draftTeamAbbrev: so.Mapped[str] = so.mapped_column(nullable=True)
+    draftRound: so.Mapped[int] = so.mapped_column(nullable=True)
+    draftPickInRound: so.Mapped[int] = so.mapped_column(nullable=True)
+    draftOverallPick: so.Mapped[str] = so.mapped_column(nullable=True)
+    inHHOF: so.Mapped[int] = so.mapped_column()
+    metaDateTime: so.Mapped[datetime] = so.mapped_column(default = lambda: datetime.now(timezone.utc))
+
+    def __repr__(self):
+        return f"<{self.firstName} {self.lastName}>"
