fde8cda4b8ad0ee44a44d570d91b8df436fd7fca
jules-collard <jules_collard@yahoo.co.uk>
2025-07-30 12:44:54 -0400

    CORS error fix

--- backend/app/api/players.py
+++ backend/app/api/players.py
@@ -1,10 +1,8 @@
 from app.api import bp, db
-from app.api.errors import bad_request
 from app.models import Player
-
-from flask import request
-import sqlalchemy as sa
+from flask_cors import cross_origin
 
 @bp.route('/players/<int:id>', methods=['GET'])
+@cross_origin()
 def get_player(id):
     return db.get_or_404(Player, id).to_dict()
--- backend/app/api/teams.py
+++ backend/app/api/teams.py
@@ -1,11 +1,8 @@
 from app.api import bp, db
-from app.api.errors import bad_request
 from app.models import Team
-from json import loads
-
-from flask import request
-import sqlalchemy as sa
+from flask_cors import cross_origin
 
 @bp.route('/teams/<int:id>', methods=['GET'])
+@cross_origin()
 def get_team(id):
     return db.get_or_404(Team, id).to_dict()
