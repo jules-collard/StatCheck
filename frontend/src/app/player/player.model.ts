41d4c770644bead25b7c60fc3fa85ca97b3ec112
jules-collard <jules_collard@yahoo.co.uk>
2025-07-30 12:47:22 -0400

    player component

---
+++ frontend/src/app/player/player.model.ts
@@ -0,0 +1,24 @@
+export type Player = {
+    id: number;
+    isActive: boolean;
+    currentTeamID: number | null;
+    firstName: string;
+    lastName: string;
+    sweaterNumber: number | null;
+    position: string;
+    headshot: string | null;
+    heroImage: string | null;
+    heightInInches: number | null;
+    heightInCentimeters: number | null;
+    weightInPounds: number | null;
+    weightInKilograms: number | null;
+    birthDate: string; // ISO date string
+    birthCity: string | null;
+    birthCountry: string | null;
+    shootsCatches: string;
+    draftYear: number | null;
+    draftTeamAbbrev: string | null;
+    draftRound: number | null;
+    draftPickInRound: number | null;
+    draftOverallPick: number | null;
+};
