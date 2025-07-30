9f024ad22ed9e3dbdcd9d2b6104094b9037388a7
jules-collard <jules_collard@yahoo.co.uk>
2025-07-29 22:56:47 -0400

    Delete src folder

--- src/scraping/uploads.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from scrapers import scrape_teams
-import requests
-
-teams: pd.DataFrame = scrape_teams()
-team = teams.loc[0].to_json()
-
-requests.post("http://127.0.0.1:5000/api/teams", json=team)
