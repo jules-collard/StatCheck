da6975f5808308e6c782aa526eaac66de7ef1295
jules-collard <jules_collard@yahoo.co.uk>
2025-07-29 23:15:03 -0400

    HTTP client

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  protected readonly title = signal('StatCheck');
+
+  private httpClient = inject(HttpClient);
 }
