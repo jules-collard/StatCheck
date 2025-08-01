f389deafdb0743a46ae318fdcd7961e9f140d1dd
jules-collard <jules_collard@yahoo.co.uk>
2025-08-01 11:57:23 -0400

    Response logging

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
-import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
-  console.log('[Outgoing Request]')
-  console.log(request)
-  return next(request)
+  console.log('[Outgoing Request]');
+  console.log(request);
+  return next(request).pipe(
+    tap({
+      next: event => {
+        if (event.type === HttpEventType.Response) {
+          console.log('[Incoming Response]');
+          console.log(event.status);
+          console.log(event.body);
+        }
+      }
+    })
+  );
 }
 
 bootstrapApplication(App, {
