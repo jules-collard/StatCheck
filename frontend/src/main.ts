ddbeef7decd367ce654f1cd360ffbb530e8208c3
jules-collard <jules_collard@yahoo.co.uk>
2025-08-01 11:50:25 -0400

    Logging interceptor

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+
+function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
+  console.log('[Outgoing Request]')
+  console.log(request)
+  return next(request)
+}
 
 bootstrapApplication(App, {
-  providers: [provideHttpClient()]
+  providers: [provideHttpClient(withInterceptors([loggingInterceptor]))]
 })
   .catch((err) => console.error(err));
