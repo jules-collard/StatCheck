df21ef46dd915e74bff2403251ce56909dba769e
jules-collard <jules_collard@yahoo.co.uk>
2025-08-01 11:58:42 -0400

    Moved HTTP logic to player service

--- frontend/src/app/player-page/player-page.ts
+++ frontend/src/app/player-page/player-page.ts
@@ -1,30 +1,32 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, DestroyRef, inject, input, signal } from '@angular/core';
+import { Component, DestroyRef, inject, input, OnInit, signal } from '@angular/core';
 
 import { Player } from './player.model';
+import { PlayerInfo } from './player-info/player-info';
+import { PlayerService } from './player.service';
 
 @Component({
   selector: 'app-player-page',
-  imports: [],
+  imports: [PlayerInfo],
   templateUrl: './player-page.html',
   styleUrl: './player-page.css'
 })
-export class PlayerPage {
-  private httpClient = inject(HttpClient);
+export class PlayerPage implements OnInit {;
   private destroyRef = inject(DestroyRef);
+  private playerService = inject(PlayerService)
 
-  id = input.required<number>()
-  playerData = signal<Player | undefined>(undefined)
+  id = input.required<number>();
+  playerData = signal<Player | null>(null)
 
   ngOnInit() {
-    const subscription = this.httpClient.get<Player>('http://localhost:5000/api/players/'+ this.id).subscribe({
+    const subscription = this.playerService.fetchPlayerData(this.id()).subscribe({
       next: (resData) => {
-        this.playerData.set(resData)
+        this.playerData.set(resData);
       }
     });
 
     this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe()
+      subscription.unsubscribe();
     });
   }
 }
---
+++ frontend/src/app/player-page/player.service.ts
@@ -0,0 +1,14 @@
+import { inject, Injectable } from "@angular/core";
+import { Player } from "./player.model";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PlayerService {
+    private httpClient = inject(HttpClient)
+
+    fetchPlayerData(id: number) {
+        return this.httpClient.get<Player>('http://localhost:5000/api/players/' + id)
+    }
+}
