9b788583010cc6776bb86425ef7d15918859e709
jules-collard <jules_collard@yahoo.co.uk>
2025-08-01 14:56:04 -0400

    Age calculation

--- frontend/src/app/player-page/player-info/player-info.ts
+++ frontend/src/app/player-page/player-info/player-info.ts
@@ -1,14 +1,21 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { Player } from '../player.model';
 import { MatCardModule } from '@angular/material/card';
-import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-player-info',
-  imports: [MatCardModule, DatePipe],
+  imports: [MatCardModule],
   templateUrl: './player-info.html',
   styleUrl: './player-info.css'
 })
 export class PlayerInfo {
   playerData = input.required<Player>()
+  playerAge = computed(() => {
+    const today = new Date();
+    const birthDate = new Date(this.playerData().birthDate);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) { age--; }
+    return age;
+  })
 }
