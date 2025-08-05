0651f1519fccd650d61412f4bae6522161bac9a8
jules-collard <jules_collard@yahoo.co.uk>
2025-08-04 20:11:27 -0400

    Moved info HTTP logic to player-info component

--- frontend/src/app/player-page/player-info/player-info.ts
+++ frontend/src/app/player-page/player-info/player-info.ts
@@ -1,8 +1,9 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input, signal } from '@angular/core';
 import { Player } from '../player.model';
 import { MatCardModule } from '@angular/material/card';
 import { AgePipe } from '../../pipes/age.pipe';
 import { HeightPipe } from '../../pipes/height-pipe';
+import { PlayerService } from '../player.service';
 
 @Component({
   selector: 'app-player-info',
@@ -11,5 +12,26 @@ import { HeightPipe } from '../../pipes/height-pipe';
   styleUrl: './player-info.css'
 })
 export class PlayerInfo {
-  playerData = input.required<Player>()
+  private destroyRef = inject(DestroyRef);
+  private playerService = inject(PlayerService)
+
+  id = input.required<number>();
+  playerData = signal<Player | null>(null)
+  error = signal('')
+
+  ngOnInit() {
+    const subscription = this.playerService.fetchPlayerData(this.id()).subscribe({
+      next: (resData) => {
+        this.playerData.set(resData);
+      },
+      error: (error) => {
+        console.log(error)
+        this.error.set("Error fetching player data.")
+      }
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
--- frontend/src/app/player-page/player-page.ts
+++ frontend/src/app/player-page/player-page.ts
@@ -1,37 +1,14 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, DestroyRef, inject, input, OnInit, signal } from '@angular/core';
+import { Component, input } from '@angular/core';
 
-import { Player } from './player.model';
 import { PlayerInfo } from './player-info/player-info';
-import { PlayerService } from './player.service';
+import { PlayerStats } from './player-stats/player-stats';
 
 @Component({
   selector: 'app-player-page',
-  imports: [PlayerInfo],
+  imports: [PlayerInfo, PlayerStats],
   templateUrl: './player-page.html',
   styleUrl: './player-page.css'
 })
-export class PlayerPage implements OnInit {;
-  private destroyRef = inject(DestroyRef);
-  private playerService = inject(PlayerService)
-
-  id = input.required<number>();
-  playerData = signal<Player | null>(null)
-  error = signal('')
-
-  ngOnInit() {
-    const subscription = this.playerService.fetchPlayerData(this.id()).subscribe({
-      next: (resData) => {
-        this.playerData.set(resData);
-      },
-      error: (error) => {
-        console.log(error)
-        this.error.set("Error fetching player data.")
-      }
-    });
-
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
-    });
-  }
+export class PlayerPage {;
+  id = input.required<number>()
 }
