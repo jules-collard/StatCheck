635ed0aa0ad28556df16967e7fc436024f37f4a6
jules-collard <jules_collard@yahoo.co.uk>
2025-08-13 17:51:16 -0400

    Added gameType query parameter

--- backend/app/api/players.py
+++ backend/app/api/players.py
@@ -1,6 +1,7 @@
 from app.api import bp, db
 from app.models import Player, Team
 from flask_cors import cross_origin
+from flask import request
 
 from sqlalchemy.sql import text
 import sqlalchemy as sa
@@ -29,11 +30,12 @@ def get_player(id):
 @cross_origin()
 def get_player_stats(id):
     db.get_or_404(Player, id)
+    gameType = int(request.args.get('gameType', 2))
 
     with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../sql', 'player_season_totals.sql')) as f:
         query = f.read()
 
-    query_result = db.session.execute(text(query), {"playerID": id, "gameType": 2}).mappings().all()
+    query_result = db.session.execute(text(query), {"playerID": id, "gameType": gameType}).mappings().all()
     results = [dict(row) for row in query_result]
     
     teams = {}
