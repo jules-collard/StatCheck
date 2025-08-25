114b6500b54c47bdf6c0f7332b86b3d7d0b10b42
jules-collard <jules_collard@yahoo.co.uk>
2025-08-25 14:09:43 -0400

    Predictions

--- backend/app/analytics/expected_goals/testing.py
+++ backend/app/analytics/expected_goals/testing.py
@@ -1,17 +1,28 @@
 import os
 import xgboost as xgb
 import matplotlib.pyplot as plt
+from sklearn.metrics import log_loss, roc_auc_score, roc_curve
 
-mod = xgb.XGBClassifier()
-mod.load_model(os.path.join(os.path.dirname(__file__), "xg_model.json"))
+from .cleaning import get_clean_data
 
-xgb.plot_importance(mod)
-plt.show()
+def load_model():
+    mod = xgb.XGBClassifier()
+    mod.load_model(os.path.join(os.path.dirname(__file__), "xg_model.json"))
+    return mod
 
-# feature_importance = xg.get_booster().get_score(importance_type = 'weight')
+def predict(mod):
+    X_test, y_test = get_clean_data(20162017, 20162017)
+    y_pred = mod.predict_proba(X_test)
+    print(y_pred[:,1])
+    X_test['isGoal'] = y_test
+    X_test['xg'] = y_pred[:,1]
 
-# keys = list(feature_importance.keys())
-# values = list(feature_importance.values())
+    X_test.to_pickle(os.path.join(os.path.dirname(__file__), "predictions.pkl"))
 
-# importance = pd.DataFrame(data = values, index=keys, columns=["score"]).sort_values(by="score", ascending=False)
-# print(importance)
+def model_importance(mod):
+    xgb.plot_importance(mod)
+    plt.show()
+
+if __name__ == "__main__":
+    mod = load_model()
+    predict(mod)
