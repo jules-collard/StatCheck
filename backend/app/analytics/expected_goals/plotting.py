e4f2ce6ec737ccccae7230764f7d097672762094
jules-collard <jules_collard@yahoo.co.uk>
2025-08-28 12:46:56 -0400

    Functions for heatmaps

--- backend/app/analytics/expected_goals/plotting.py
+++ backend/app/analytics/expected_goals/plotting.py
@@ -1,9 +1,11 @@
 from hockey_rink import NHLRink
 import matplotlib.pyplot as plt
+import xgboost as xgb
 import pandas as pd
+import seaborn as sns
 import os
 
-predictions = pd.read_pickle(os.path.join(os.path.dirname(__file__), "predictions.pkl"))
+from .testing import load_model
 
 rink = NHLRink(net={"visible": False})
 
@@ -11,52 +13,73 @@ common_params = {
     'x': 'xStd',
     'y': 'yStd',
     'values': 'xg',
-    'data': predictions,
-    'draw_kw': {'display_range': 'ozone', 'rotation': 270}
+    "draw_kw": {"display_range": "ozone", "rotation": 270}
 }
 
-def plot_hexbin(ax):
+def plot_hexbin(ax, data, cmap):
     rink.hexbin(
         gridsize=(14, 9),
         extent=(25, 100, -42.5, 42.5),
         vmax=0.25,
         ax=ax,
-        cmap='magma',
+        data=data,
+        cmap=cmap,
         **common_params,
     )
 
-    min_color = plt.get_cmap("viridis")(0)
+    min_color = plt.get_cmap(cmap)(0)
 
     half_length = 200 / 2
     half_width = 83 / 2
 
     rink.plot_fn(
         ax.fill,
-
-        # Filling the entire rink for simplicity.
         x=[-half_length, half_length, half_length, -half_length],
         y=[-half_width, -half_width, half_width, half_width],
-
         color=min_color,
         zorder=2.5,
-
-        # Matplotlib doesn't allow x and y to be passed as keyword parameters.
-        # We get around this by telling plot_fn that they're positional
-        # arguments.
         position_args=["x", "y"],
     )
 
-def plot_contourf(ax):
-    rink.contourf(
-        nbins=8,
-        levels=30,
-        plot_range="ozone",
-        cmap="bwr",
+def plot_shot_density(ax, data, cmap):
+    rink.plot_fn(
+        sns.kdeplot,
+        x='xStd', y='yStd',
+        cmap=cmap, fill=True, alpha=0.8,
+        levels=20,
+        data=data,
         ax=ax,
-        **common_params,
+        draw_kw={"display_range": "ozone", "rotation": 270}
     )
 
-fig, axs = plt.subplots(figsize=(12,4))
-plot_hexbin(axs)
+def plot_shot_locations(es_predictions, pp_predictions, cmap='plasma'):
+    fig, axs = plt.subplots(1,2)
+    plot_shot_density(axs[0], es_predictions, cmap=cmap)
+    axs[0].set_title('Even Strength')
+    plot_shot_density(axs[1], pp_predictions, cmap=cmap)
+    axs[1].set_title('Powerplay')
 
-plt.show()
+    fig.suptitle("Shot Locations", fontweight='bold', fontsize=16)
+
+    plt.show()
+
+def plot_heatmaps(es_predictions, pp_predictions, cmap='plasma'):
+    fig, axs = plt.subplots(1, 2)
+    plot_hexbin(axs[0], es_predictions, cmap=cmap)
+    axs[0].set_title('Even Strength')
+    plot_hexbin(axs[1], pp_predictions, cmap=cmap)
+    axs[1].set_title('Powerplay')
+
+    fig.suptitle("xG by Location", fontweight="bold", fontsize=16)
+
+    plt.show()
+
+def plot_feature_importance(name: str):
+    mod = load_model(name)
+    xgb.plot_importance(mod)
+
+if __name__ == "__main__":
+    es_predictions = pd.read_pickle(os.path.join(os.path.dirname(__file__), "models", "ES_predictions.pkl"))
+    pp_predictions = pd.read_pickle(os.path.join(os.path.dirname(__file__), "models", "PP_predictions.pkl"))
+
+    plot_shot_locations(es_predictions, pp_predictions)
