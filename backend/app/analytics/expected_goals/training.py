38c9de8153ca1dcbd8f72d5b6790014211d17edc
jules-collard <jules_collard@yahoo.co.uk>
2025-09-06 18:16:51 -0400

    Remove staged tuning

--- backend/app/analytics/expected_goals/training.py
+++ backend/app/analytics/expected_goals/training.py
@@ -1,6 +1,5 @@
 import xgboost as xgb
-from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
-from scipy.stats import uniform
+from sklearn.model_selection import GridSearchCV
 import os
 
 from .cleaning import load_seasons, clean_data, transform_data
@@ -12,45 +11,35 @@ def train_model(name, X_train, y_train, model_to_load=None):
         mod.load_model(os.path.join(os.path.dirname(__file__), "models", model_to_load))
         mod.fit(X_train, y_train)
     else:
-        xgclassifier_stage1 = xgb.XGBClassifier(objective='binary:logistic',
-                                                verbosity = 1,
-                                                tree_method = 'hist',
-                                                n_jobs=None)
+        xgclassifier = xgb.XGBClassifier(objective='binary:logistic',
+                                        verbosity = 1,
+                                        subsample = 0.9,
+                                        tree_method = 'hist',
+                                        n_jobs=None)
 
-        distributions = {'eta': uniform(loc=0, scale=0.3)}
-        clf_stage1 = RandomizedSearchCV(xgclassifier_stage1, distributions, random_state=5, scoring='neg_log_loss', n_jobs=1, verbose=2)
-        clf_stage1.fit(X_train, y_train)
-        eta = clf_stage1.best_params_['eta']
-
-        xgclassifier_stage2 = xgb.XGBClassifier(objective='binary:logistic',
-                                                verbosity = 1,
-                                                eta = eta,
-                                                tree_method = 'hist',
-                                                n_jobs=None)
-        
         param_grid = {
-            'gamma': [0, 0.01, 0.1],
-            'max_depth': [3, 4, 5, 6],
-            'subsample': [0.9, 1],
+            'eta': [0.05, 0.08, 0.1, 0.2],
+            'max_depth': [4, 5, 6, 7],
             'min_child_weight': [0.5, 1, 1.5]
         }
 
-        clf_stage2 = GridSearchCV(xgclassifier_stage2, param_grid, scoring='neg_log_loss', n_jobs=1, verbose=2)
-        clf_stage2.fit(X_train, y_train)
-        mod = clf_stage2.best_estimator_
+        clf = GridSearchCV(xgclassifier, param_grid, scoring='neg_log_loss', n_jobs=1, verbose=2)
+        clf.fit(X_train, y_train)
+        mod = clf.best_estimator_
 
     with open(os.path.join(os.path.dirname(__file__), "models", f"{name}_performance.txt"), "w") as f:
         f.write(f"{name} Stats: \n")
-        f.write(f"In-Sample Log-Loss (CV) = {clf_stage2.best_score_}\n")
+        f.write(f"Log-Loss (CV) = {clf.best_score_}\n")
         for param, value in mod.get_params().items():
-            f.write(f"{param}: {value}\n")
+            if param in ['eta', 'max_depth', 'min_child_weight']:
+                f.write(f"{param}: {value}\n")
     
     mod.save_model(os.path.join(os.path.dirname(__file__), "models", f"{name}.json"))
     
     return mod
 
 if __name__ == "__main__":
-    data = load_seasons(20102011, 20162017)
+    data = load_seasons(20142015, 20242025)
     es_data, pp_data, sh_data = clean_data(data)
     
     # Even Strength Model
