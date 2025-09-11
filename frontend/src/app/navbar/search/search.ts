fa8ae46abf834f5b49cd3d75bbd7c4f818932178
jules-collard <jules_collard@yahoo.co.uk>
2025-09-11 14:47:47 -0400

    Remove unused imports

--- frontend/src/app/navbar/search/search.ts
+++ frontend/src/app/navbar/search/search.ts
@@ -1,12 +1,12 @@
-import { Component, computed, inject, output, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms'
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { PlayerListService } from '../../search-page/player-list/player-list.service';
 import { SearchService } from '../../search-page/search.service';
 
 @Component({
   selector: 'app-search',
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule],
   templateUrl: './search.html',
   styleUrl: './search.css'
 })
