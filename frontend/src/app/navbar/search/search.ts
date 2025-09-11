0e28892693cfd3efb82acb041727251f2f579cfe
jules-collard <jules_collard@yahoo.co.uk>
2025-09-11 14:45:54 -0400

    Use router instead of routerlink

--- frontend/src/app/navbar/search/search.ts
+++ frontend/src/app/navbar/search/search.ts
@@ -1,6 +1,6 @@
 import { Component, computed, inject, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms'
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { PlayerListService } from '../../search-page/player-list/player-list.service';
 import { SearchService } from '../../search-page/search.service';
 
@@ -13,10 +13,12 @@ import { SearchService } from '../../search-page/search.service';
 export class Search {
   private playerListService = inject(PlayerListService)
   private searchService = inject(SearchService)
+  private router = inject(Router)
   enteredPlayerName = '';
 
   onFindPlayer() {
     this.playerListService.setNameToSearch(this.enteredPlayerName)
     this.searchService.firstPage()
+    this.router.navigate(['search'])
   }
 }
