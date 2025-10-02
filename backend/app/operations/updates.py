a5a8f88ec1c4a8b69a37f9f261f9f74c1be22a64
Jules Lanari-Collard <jules_collard@yahoo.co.uk>
2025-10-02 21:52:01 +0100

    Fix game CLI commands

--- backend/app/operations/updates.py
+++ backend/app/operations/updates.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import click
 
@@ -16,30 +16,35 @@ def games():
     pass
 
 @games.command('import')
-@click.option('-i', '--id', type=int)
-@click.option('-x', '--xg', type=bool)
+@click.option('-i', '--id', prompt=True, type=int)
+@click.option('-x', '--xg', default=True, type=bool, help="Calculate XG for Game Events")
 def import_game(id: int, xg: bool):
+    if id is None:
+        click.Abort()
     g.import_game(id, calc_xg=xg)
 
 @games.command('import-date')
-@click.option('-d', '--date', default=datetime.today().strftime('%Y-%m-%d'), type=str)
+@click.option('-d', '--date', default=(datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d'), type=str)
 def import_games_date(date: str):
     g.import_games_on_date(date)
 
 @games.command('import-range')
-@click.option('-d', '--date', type=(str,str))
-def import_games_range(date_tup: tuple[str]):
-    g.import_games_date_range(date_tup[0], date_tup[1])
+@click.argument('start_date', type=str)
+@click.argument('end_date', type=str)
+def import_games_range(start_date, end_date):
+    g.import_games_date_range(start_date, end_date)
 
 @games.command('fix')
 def fix_games():
     g.import_games_from_errors()
 
 @games.command('remove')
-@click.option('-i', '--id', type=int)
+@click.option('-i', '--id', prompt=True, type=int)
 def remove_game(id: int):
+    if id is None:
+        click.Abort()
     g.remove_game(id)
 
 if __name__ == '__main__':
-    with app.app_context():
-        cli()
+    app.app_context().push()
+    cli()
