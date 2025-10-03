7b9d363b74cf461fd40e68fcbc5d14a21dbb67db
Jules Lanari-Collard <jules_collard@yahoo.co.uk>
2025-10-03 18:49:10 +0100

    Sorting by season

--- frontend/src/app/player-page/skater-totals-table/skater-totals-table.ts
+++ frontend/src/app/player-page/skater-totals-table/skater-totals-table.ts
@@ -1,4 +1,4 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, input, signal } from '@angular/core';
 import { SkaterStats } from '../skater-stats.model';
 import { SeasonPipe } from '../../pipes/season.pipe';
 import { AwardBadge } from './award-badge/award-badge';
@@ -16,6 +16,25 @@ import { BoldRecordPipe } from '../../pipes/bold-record.pipe';
 export class SkaterTotalsTable {
   seasonStats = input.required<SkaterStats[]>();
   awards = input<Award[]>([]);
+
+  sortSeasons = signal<'asc'|'desc'|null>('asc');
+
+  stats = computed<SkaterStats[]>(() => {
+    if (this.sortSeasons() === 'asc') {
+      return this.seasonStats().sort((a,b) => a.season - b.season);
+    } else if (this.sortSeasons() === 'desc') {
+      return this.seasonStats().sort((a,b) => b.season - a.season);
+    }
+    return this.seasonStats();
+  })
+
+  toggleSort(currentVal: 'asc'|'desc'|null) {
+    if (currentVal === 'desc') {
+        return 'asc';
+    } else {
+      return 'desc';
+    }
+  }
 
   getSeasonAwards(season: number) {
     let seasonAwards = this.awards().filter((award) => {
