cea3d1d3184d7e3403b858aef7804ad814e6be0a
Jules Lanari-Collard <jules_collard@yahoo.co.uk>
2025-10-03 18:49:24 +0100

    Remove data_updates module

--- backend/statcheck_backend.py
+++ backend/statcheck_backend.py
@@ -1,8 +1,7 @@
 from app import app, db
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import data_updates as du
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'sa':sa, 'so':so, 'db':db, 'du':du }
+    return {'sa':sa, 'so':so, 'db':db }
