0e08f28171919c0068ee33f028535c39845160d7
Jules Lanari-Collard <jules_collard@yahoo.co.uk>
2025-10-05 11:36:36 +0100

    Season sorting in on-ice table (doesn't work for % categories - unsure why)

--- frontend/src/app/player-page/skater-onice-table/skater-onice-table.ts
+++ frontend/src/app/player-page/skater-onice-table/skater-onice-table.ts
@@ -1,14 +1,31 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, effect, inject, input, OnInit } from '@angular/core';
 import { SkaterStats } from '../skater-stats.model';
 import { SeasonPipe } from '../../pipes/season.pipe';
 import { DecimalPipe } from '@angular/common';
+import { SortConfig, TableSortService } from '../../shared/table-sort.service';
 
 @Component({
   selector: 'app-skater-onice-table',
   imports: [SeasonPipe, DecimalPipe],
   templateUrl: './skater-onice-table.html',
-  styleUrl: './skater-onice-table.css'
+  styleUrl: './skater-onice-table.css',
+  providers: [TableSortService]
 })
-export class SkaterOnIceTable {
-  seasonStats = input.required<SkaterStats[]>()
+export class SkaterOnIceTable implements OnInit {
+  sortService = inject(TableSortService<SkaterStats>);
+
+  seasonStats = input.required<SkaterStats[]>();
+  
+  statsEffect = effect(() => {
+    this.sortService.setData(this.seasonStats());
+  })
+  stats = computed<SkaterStats[]>(() => this.sortService.sortedData())
+  
+  ngOnInit(): void {
+    const sortConfig: SortConfig<SkaterStats> = {
+      season: (item) => item.season
+    }
+
+    this.sortService.setSortConfig(sortConfig);
+  }
 }
