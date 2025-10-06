81c0e12def8a0d8dd2aa9db1d6878f0004f133bb
Jules Lanari-Collard <jules_collard@yahoo.co.uk>
2025-10-06 20:38:18 +0100

    Add leaderboards endpoint

---
+++ backend/app/api/leaderboards.py
@@ -0,0 +1,23 @@
+import os
+import json
+
+from flask_cors import cross_origin
+from flask import request, abort
+
+import app
+from app.api import bp
+
+@bp.route('/leaderboards/<int:season>/skaters', methods=['GET'])
+@cross_origin()
+def get_skater_leaderboards(season: int, gameType: int = 2):
+    try:
+        gameType = int(request.args.get('gameType', 2))
+        if gameType not in [2,3]:
+            raise ValueError
+        with open(os.path.join(app.ROOT_PATH, 'data', 'leaderboards', f'skaters_{season}_{gameType}.json')) as f:
+            leaderboard = json.load(f)
+            return json.dumps(leaderboard)
+    except ValueError:
+        abort(400)
+    except FileNotFoundError:
+        abort(404)
