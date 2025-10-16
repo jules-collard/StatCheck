b18e5f5795176fdec26b54457605a402bf6e4eec
Jules Lanari-Collard <jules_collard@yahoo.co.uk>
2025-10-16 18:26:25 +0100

    Added link to player pages

--- frontend/src/app/leaderboard-page/skater-table/skater-table.ts
+++ frontend/src/app/leaderboard-page/skater-table/skater-table.ts
@@ -1,6 +1,7 @@
-import { Component, computed, input, signal } from '@angular/core';
+import { Component, computed, inject, input, signal } from '@angular/core';
 import { SkaterLeaderboardItem } from '../skater-leaderboard-item.model';
 import { timeOnIcePipe } from '../../pipes/timeOnIce.pipe';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-skater-table',
@@ -9,5 +10,11 @@ import { timeOnIcePipe } from '../../pipes/timeOnIce.pipe';
   styleUrl: './skater-table.css'
 })
 export class SkaterTable {
+  private router = inject(Router)
+
   skaters = input.required<SkaterLeaderboardItem[]>();
+
+  navigateToPlayer(id: number) {
+    this.router.navigate(['players', id])
+  }
 }
