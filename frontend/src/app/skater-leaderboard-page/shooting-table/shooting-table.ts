96add77f49c91e0ec9bf3b4a76fe80da3e3bd83e
Jules Lanari-Collard <jules_collard@yahoo.co.uk>
2025-10-26 21:37:06 +0000

    Add player position to tables

--- frontend/src/app/skater-leaderboard-page/shooting-table/shooting-table.ts
+++ frontend/src/app/skater-leaderboard-page/shooting-table/shooting-table.ts
@@ -3,10 +3,11 @@ import { Router } from '@angular/router';
 import { SkaterLeaderboardItem } from '../skater-leaderboard-item.model';
 import { TableSortService } from '../../shared/table-sort.service';
 import { DecimalPipe } from '@angular/common';
+import { PositionPipe } from '../../pipes/position.pipe';
 
 @Component({
   selector: 'app-shooting-table',
-  imports: [DecimalPipe],
+  imports: [DecimalPipe, PositionPipe],
   templateUrl: './shooting-table.html',
   styleUrl: './shooting-table.css'
 })
--- frontend/src/app/skater-leaderboard-page/skater-table/skater-table.ts
+++ frontend/src/app/skater-leaderboard-page/skater-table/skater-table.ts
@@ -3,10 +3,11 @@ import { SkaterLeaderboardItem } from '../skater-leaderboard-item.model';
 import { timeOnIcePipe } from '../../pipes/timeOnIce.pipe';
 import { Router } from '@angular/router';
 import { TableSortService } from '../../shared/table-sort.service';
+import { PositionPipe } from '../../pipes/position.pipe';
 
 @Component({
   selector: 'app-skater-table',
-  imports: [timeOnIcePipe],
+  imports: [timeOnIcePipe, PositionPipe],
   templateUrl: './skater-table.html',
   styleUrl: './skater-table.css'
 })
