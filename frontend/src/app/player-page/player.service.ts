73b0864803f470b4f28c75b1c4772a46f4e3a110
Jules Lanari-Collard <jules_collard@yahoo.co.uk>
2025-10-27 21:49:42 +0000

    Remove resolvers and use player service

--- frontend/src/app/player-page/player.service.ts
+++ frontend/src/app/player-page/player.service.ts
@@ -1,38 +1,40 @@
-import { Injectable, signal } from "@angular/core";
+import { inject, Injectable, signal } from "@angular/core";
 import { Player } from "./player.model";
 import { httpResource } from "@angular/common/http";
 import { SkaterStats } from "./skater-stats.model";
 import { GoalieStats } from "./goalie-stats.model";
+import { GlobalConfigService } from "../shared/global-config.service";
 
-@Injectable({
-    providedIn: 'root'
-})
+@Injectable()
 export class PlayerService {
+    configService = inject(GlobalConfigService)
 
-    private PLAYER_URL = 'http://localhost:5000/api/players'
-
-    private shouldFetch = signal<boolean>(false)
+    private PLAYER_URL = `${this.configService.backendURL}/players`
     private playerID = signal<number | null>(null);
 
     playerData = httpResource<Player>(() => {
-        const shouldFetch = this.shouldFetch();
-        return shouldFetch ? `${this.PLAYER_URL}/${this.playerID()}` : undefined;
+        if (this.playerID()) {
+            return `${this.PLAYER_URL}/${this.playerID()}`;
+        } else {
+            return undefined;
+        }
     });
     regSeasonStats = httpResource<SkaterStats[] | GoalieStats[]>(() => {
-        const shouldFetch = this.shouldFetch();
-        return shouldFetch ? `${this.PLAYER_URL}/${this.playerID()}/stats?gameType=2` : undefined
+        if (this.playerID()) {
+            return `${this.PLAYER_URL}/${this.playerID()}/stats?gameType=2`;
+        } else {
+            return undefined;
+        }
     });
     postSeasonStats = httpResource<SkaterStats[] | GoalieStats[]>(() => {
-        const shouldFetch = this.shouldFetch();
-        return shouldFetch ? `${this.PLAYER_URL}/${this.playerID()}/stats?gameType=3` : undefined
+        if (this.playerID()) {
+            return `${this.PLAYER_URL}/${this.playerID()}/stats?gameType=3`;
+        } else {
+            return undefined;
+        }
     });
 
     setPlayerID(id: number) {
         this.playerID.set(id)
-        console.log('ID Set to' + this.playerID())
-    }
-
-    fetch() {
-        this.shouldFetch.set(true)
     }
 }
