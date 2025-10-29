516de696f7472a40bf4901893b4677fea6f0ff70
Jules Lanari-Collard <jules_collard@yahoo.co.uk>
2025-10-29 22:59:16 +0000

    Delete redundant player list, filter and search service

--- frontend/src/app/search-page/player-list/player-list-item.model.ts
+++
@@ -1,8 +0,0 @@
-export type PlayerListItem = {
-    id: number;
-    fullName: string;
-    isActive: boolean;
-    position: 'G' | 'D' | 'L' | 'R' | 'C';
-    teamTriCode: string | null;
-    headshot: string;
-};
