08e853356efd8c06be7ff7b95ce6f2c41461c589
Jules Lanari-Collard <jules_collard@yahoo.co.uk>
2025-11-12 12:59:42 +0000

    Copy new models to db-updates

---
+++ db-updates/src/scrapers/teams.py
@@ -0,0 +1,20 @@
+import requests
+
+from .. import BACKEND_URL
+
+def scrape_teams():
+    url = "https://api.nhle.com/stats/rest/en/team"
+
+    response = requests.get(url)
+    response.raise_for_status()
+    response = response.json()
+    teams = [{k:team.get(k) for k in ["id", "franchiseId", "fullName", "triCode"]} for team in response.get('data')]
+
+    return teams
+
+def post_team(team: TeamBase):
+    r = requests.post(f"{BACKEND_URL}/teams/", json=team.model_dump())
+    print(r.status_code)
+
+if __name__ == "__main__":
+    print(scrape_teams())
