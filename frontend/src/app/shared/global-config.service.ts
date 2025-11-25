e4a862d1634b9f40f71e72d487bf6a3886e2ea79
Jules Lanari-Collard <jules_collard@yahoo.co.uk>
2025-11-25 20:38:27 +0000

    Use environment files - .env was not persisting in build

--- frontend/src/app/shared/global-config.service.ts
+++ frontend/src/app/shared/global-config.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from "@angular/core";
+import { environment } from "../../environments/environment";
 
 @Injectable({
     providedIn: 'root'
 })
 export class GlobalConfigService {
-    backendURL: string = `http://${import.meta.env.NG_APP_BACKEND_URL}/api`
+    backendURL: string = `http://${environment.apiUrl}/api`
     currentSeason: number = 20252026
     seasons: number[] = [20102011, 20112012, 20122013, 20132014, 20142015, 20152016, 20162017, 20172018, 20182019, 20192020, 20202021, 20212022, 20222023, 20232024, 20242025, 20252026]
 }
