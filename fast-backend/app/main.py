d60fa28a2ebac2c9054da60bd031be9f0517da5b
Jules Lanari-Collard <jules_collard@yahoo.co.uk>
2025-11-26 13:16:54 +0000

    Fixes for easy switching between dev and prod environment

--- fast-backend/app/main.py
+++ fast-backend/app/main.py
@@ -10,18 +10,19 @@ app = FastAPI()
 if app_config.environment == 'prod':
     logfire.configure(environment=app_config.environment)
     logfire.instrument_fastapi(app)
-
-origins = [
-    'http://localhost:8080',
-    'https://localhost:8080',
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_methods=["GET"],
-    allow_headers=["*"],
-)
+if app_config.environment == 'dev':
+    origins = [
+        'http://localhost:8080',
+        'https://localhost:8080',
+        'http://localhost:4200',
+        'https://localhost:4200'
+    ]
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_methods=["GET"],
+        allow_headers=["*"],
+    )
 
 app.include_router(players.router)
 app.include_router(teams.router)
@@ -31,6 +32,15 @@ app.include_router(events.router)
 app.include_router(leaderboards.router)
 app.include_router(scores.router)
 app.include_router(standings.router)
+
+app.include_router(players.router, prefix="/api")
+app.include_router(teams.router, prefix="/api")
+app.include_router(games.router, prefix="/api")
+app.include_router(admin.router, prefix="/api")
+app.include_router(events.router, prefix="/api")
+app.include_router(leaderboards.router, prefix="/api")
+app.include_router(scores.router, prefix="/api")
+app.include_router(standings.router, prefix="/api")
 
 @app.get("/")
 async def root():
